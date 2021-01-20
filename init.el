39fbbdac9d7bd15a55993747887a40e290d6fbbe
Frank Beutelschieß <frank@thesym.site>
2021-01-20 20:33:17 +0100

    include: tex

--- init.el
+++ init.el
@@ -923,7 +923,27 @@ byte-compiled from.")
 
 (fb*loadConfigFile "project/0-project.el")
 
-(fb*loadConfigFile "tex/auctex.el")
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; tex
+;;;;
+;;
+
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq-default TeX-master nil)
+
+(use-package auctex
+  :init (setq TeX-view-program-selection '((output-pdf "Zathura")))
+  :hook (LaTex-mode . lsp-deferred)
+  )
+
+(use-package auctex-latexmk
+  :config (auctex-latexmk-setup)
+  )
+
+;; (TeX-source-correlate-mode)        ; activate forward/reverse search
+;; (TeX-PDF-mode)
+;; (add-to-list 'TeX-view-program-list '("zathura" zathura-forward-search))
+;; (setq TeX-view-program-selection (quote ((output-pdf "zathura") (output-dvi "xdvi"))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; themes
 ;;;;
