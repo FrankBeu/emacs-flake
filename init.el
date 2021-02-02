73021063188fe38c57eae05f5dd3e963bda0c7c0
Frank Beutelschieß <frank@thesym.site>
2021-02-02 04:22:58 +0100

    add own-themes

--- init.el
+++ init.el
@@ -326,6 +326,83 @@ byte-compiled from.")
 
 (use-package swiper)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; themes
+;;;;
+;;
+
+;; (load-theme 'deeper-blue)
+;; (load-theme 'wombat)
+
+(use-package solaire-mode
+  :hook (after-init . solaire-global-mode))
+
+(use-package doom-themes
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ;; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ;; if nil, italics is universally disabled
+  ;; (load-theme 'doom-acario-dark t)
+  ;; (load-theme 'doom-acario-light t)
+  ;; (load-theme 'doom-challenger-deep t)
+  ;; (load-theme 'doom-city-lights t)
+  ;; (load-theme 'doom-dark+ t)                       ;; +
+ ;; (load-theme 'doom-dracula t)                     ;; +
+  ;; (load-theme 'doom-ephemeral t)
+  ;; (load-theme 'doom-fairy-floss t)
+  ;; (load-theme 'doom-gruvbox-light t)
+  ;; (load-theme 'doom-gruvbox t)                     ;; +
+  ;; (load-theme 'doom-henna t)
+  ;; (load-theme 'doom-horizon t)
+  ;; (load-theme 'doom-Iosvkem t)
+  ;; (load-theme 'doom-laserwave t)
+  ;; (load-theme 'doom-manegarm t)
+  ;; (load-theme 'doom-material t)                    ;; +
+  ;; (load-theme 'doom-molokai t)
+  ;; (load-theme 'doom-monokai-classic t)
+  ;; (load-theme 'doom-monokai-pro t)
+  ;; (load-theme 'doom-monokai-spectrum t)
+  ;; (load-theme 'doom-moonlight t)
+  ;; (load-theme 'doom-nord-light t)
+  ;; (load-theme 'doom-nord t)
+  ;; (load-theme 'doom-nova t)
+  ;; (load-theme 'doom-oceanic-next t)
+  ;; (load-theme 'doom-old-hope t)
+  ;; (load-theme 'doom-one-light t)
+  ;; (load-theme 'doom-one t)                         ;; +
+  ;; (load-theme 'doom-opera-light t)
+  ;; (load-theme 'doom-opera t)
+  ;; (load-theme 'doom-outrun-electric t)
+  ;; (load-theme 'doom-palenight t)
+  ;; (load-theme 'doom-peacock t)
+  ;; (load-theme 'doom-rouge t)
+  ;; (load-theme 'doom-snazzy t)
+  ;; (load-theme 'doom-solarized-dark t)              ;; +
+  ;; (load-theme 'doom-solarized-light t)
+  ;; (load-theme 'doom-sourcerer t)
+  ;; (load-theme 'doom-spacegrey t)
+  ;; (load-theme 'doom-tomorrow-day t)
+  ;; (load-theme 'doom-tomorrow-night t)
+  ;; (load-theme 'doom-vibrant t)
+  ;; (load-theme 'doom-wilmersdorf t)
+  ;; (load-theme 'doom-zenburn t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-colors") ;; use the colorful treemacs theme
+  (doom-themes-treemacs-config)
+
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config)
+  )
+
+(add-to-list 'custom-theme-load-path (expand-file-name "themes/themes" user-emacs-directory))
+;; (setq doom-theme 'fb-doom-dracula)
+(load-theme 'fb-doom-dracula t)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; evil
 ;;;;
 ;;
@@ -1128,80 +1205,6 @@ an argument, unconditionally call `org-insert-SUBheading'."
 ;; (add-to-list 'TeX-view-program-list '("zathura" zathura-forward-search))
 ;; (setq TeX-view-program-selection (quote ((output-pdf "zathura") (output-dvi "xdvi"))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; themes
-;;;;
-;;
-
-;; (load-theme 'deeper-blue)
-;; (load-theme 'wombat)
-
-;; (load-theme 'zerodark 'no-confirm)
-;; (zerodark-setup-modeline-format)
-
-(use-package doom-themes
-  :config
-  ;; Global settings (defaults)
-  (setq doom-themes-enable-bold t    ;; if nil, bold is universally disabled
-        doom-themes-enable-italic t) ;; if nil, italics is universally disabled
-  ;; (load-theme 'doom-acario-dark t)
-  ;; (load-theme 'doom-acario-light t)
-  ;; (load-theme 'doom-challenger-deep t)
-  ;; (load-theme 'doom-city-lights t)
-  ;; (load-theme 'doom-dark+ t)                       ;; +
-  (load-theme 'doom-dracula t)                     ;; +
-  ;; (load-theme 'doom-ephemeral t)
-  ;; (load-theme 'doom-fairy-floss t)
-  ;; (load-theme 'doom-gruvbox-light t)
-  ;; (load-theme 'doom-gruvbox t)                     ;; +
-  ;; (load-theme 'doom-henna t)
-  ;; (load-theme 'doom-horizon t)
-  ;; (load-theme 'doom-Iosvkem t)
-  ;; (load-theme 'doom-laserwave t)
-  ;; (load-theme 'doom-manegarm t)
-  ;; (load-theme 'doom-material t)                    ;; +
-  ;; (load-theme 'doom-molokai t)
-  ;; (load-theme 'doom-monokai-classic t)
-  ;; (load-theme 'doom-monokai-pro t)
-  ;; (load-theme 'doom-monokai-spectrum t)
-  ;; (load-theme 'doom-moonlight t)
-  ;; (load-theme 'doom-nord-light t)
-  ;; (load-theme 'doom-nord t)
-  ;; (load-theme 'doom-nova t)
-  ;; (load-theme 'doom-oceanic-next t)
-  ;; (load-theme 'doom-old-hope t)
-  ;; (load-theme 'doom-one-light t)
-  ;; (load-theme 'doom-one t)                         ;; +
-  ;; (load-theme 'doom-opera-light t)
-  ;; (load-theme 'doom-opera t)
-  ;; (load-theme 'doom-outrun-electric t)
-  ;; (load-theme 'doom-palenight t)
-  ;; (load-theme 'doom-peacock t)
-  ;; (load-theme 'doom-rouge t)
-  ;; (load-theme 'doom-snazzy t)
-  ;; (load-theme 'doom-solarized-dark t)              ;; +
-  ;; (load-theme 'doom-solarized-light t)
-  ;; (load-theme 'doom-sourcerer t)
-  ;; (load-theme 'doom-spacegrey t)
-  ;; (load-theme 'doom-tomorrow-day t)
-  ;; (load-theme 'doom-tomorrow-night t)
-  ;; (load-theme 'doom-vibrant t)
-  ;; (load-theme 'doom-wilmersdorf t)
-  ;; (load-theme 'doom-zenburn t)
-
-
-  ;; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)
-
-  ;; Enable custom neotree theme (all-the-icons must be installed!)
-  (doom-themes-neotree-config)
-  ;; or for treemacs users
-  (setq doom-themes-treemacs-theme "doom-colors") ;; use the colorful treemacs theme
-  (doom-themes-treemacs-config)
-
-  ;; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config)
-  )
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; view
 ;;;;
 ;;
@@ -1794,6 +1797,7 @@ an argument, unconditionally call `org-insert-SUBheading'."
   "t"   '(                                              :which-key "toggles"                          :ignore t)
   "ti"  '(imenu-list-smart-toggle                       :which-key "imenu"                            )
   "tl"  '(toggle-truncate-lines                         :which-key "truncate-lines"                   )
+  "tm"  '(treemacs                                      :which-key "treemacs"                         )
   "tn"  '(display-line-numbers-mode                     :which-key "line-numbers"                     )
   "tt"  '(counsel-load-theme                            :which-key "choose theme"                     )
   "tw"  '(whitespace-mode                               :which-key "whitespace"                       )
