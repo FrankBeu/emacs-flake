19a55999746af4da406047c6cdb12214034c0c89
Frank Beutelschieß <frank@thesym.site>
2021-03-22 06:36:23 +0100

    add support for web-dev; activate nix-completion; conf agenda; misc

--- test/debugger.go
+++ test/debugger.go
@@ -1,41 +1,49 @@
-  package main
+package main
 
-  import "fmt"
+import "fmt"
 
-  type Int int
+type Int int
 
-  func (i *Int) add2() {
-          *i += 2
-  }
+func (i *Int) add2() {
+	*i += 2
+}
 
-  func (i *Int) add3() {
-          *i += 3
-  }
+func (i *Int) add3() {
+	*i += 3
+}
 
-  func (i *Int) sub7() {
-          *i -= 5
-  }
+func (i *Int) sub7() {
+	*i -= 7
+}
 
-  func main() {
-          var n Int = 0
-          n.add2()
-          n.add3()
-          n.add2()
-          n.add3()
-          n.sub7()
+func main() {
+	var n Int = 0
+	n.add2()
+	n.add3()
+	fmt.Println(n)
+	n.add2()
+	n.add3()
+	fmt.Println(n)
+	n.sub7()
 
-          n.add2()
-          n.add3()
-          n.add2()
-          n.add3()
-          n.sub7()
+	n.add2()
+	n.add3()
+	n.add2()
+	n.add3()
+	n.sub7()
 
-          n.add2()
-          n.add3()
-          n.add2()
-          n.add3()
-          n.sub7()
+	for i := 0; i < 10; i++ {
+		n.add2()
+		n.add2()
+		n.add2()
+		n.add2()
+	}
 
+	n.add2()
+	n.add3()
+	n.add2()
+	n.add3()
+	n.sub7()
 
-          fmt.Printf("%d", n)
-  }
+	fmt.Printf("%d", n)
+}
